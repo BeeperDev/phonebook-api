39e6d2f00c5679c550929e16e32ee40fcf9f79ec
Erica Layton <e.alexislayton@gmail.com>
2022-06-15 21:31:29 -0700

    add push request

--- server.js
+++ server.js
@@ -1,2 +1,77 @@
 const express = require('express')
 const app = express()
+const PORT = 3001
+
+app.use(express.json())
+
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (req,res) => {
+    const currentDate = new Date()
+    res.send(`<h2> Phonebook has info for ${persons.length} people</h2> <h2> ${currentDate}</h2>`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    let id = req.params.id                                // look inside persons array and see if id matches
+    const entry = persons.find( element => element.id == id)    // assign matching one to entry array of object
+    if(entry){
+        res.json(entry)
+    }else{
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(element => element.id != id)       // filter in all the objects that don't match that id
+    res.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0 ? Math.max(...persons.map(n => n.id)) : 0
+    return maxId +1
+}
+
+app.post('/api/persons', (req,res) => {
+    const body = req.body
+
+    const aPerson = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons.push(aPerson)
+    res.json(aPerson)
+})
+
+app.listen(PORT, () => {
+    console.log('Server is up!')
+})
